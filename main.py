17f8fb56971a5fbae1aabc13c8931b9b6fb7345a
9dfk1418 <131152872+9dfk1418@users.noreply.github.com>
2023-05-23 12:12:29 +0200

    add functions code display

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ def main():
         
         if choice == "1":
             print_quote(random_quote(quotes))
-        elif choice == "2":
-            view_quotes(quotes)
         elif choice == "3":
             add_quote(quotes, 'quotes.txt')
+        elif choice == "2":  # gestion de display_count()
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
         elif choice == "4":
             print("Good bye...")
             break
